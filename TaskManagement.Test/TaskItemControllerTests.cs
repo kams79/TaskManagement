3fb5dc42df1826dc62d554a8365764d5d51bc5b5
kemal <kams79>
2025-03-02 12:40:39 +0700

    feat: add unit test

---
+++ TaskManagement.Test/TaskItemControllerTests.cs
@@ -0,0 +1,59 @@
+using System.Net.Http.Json;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TaskManagement.Business.Models;
+using TaskManagement.Models;
+
+public class TaskItemControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public TaskItemControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetAllTaskItems_ReturnsOkResponse()
+    {
+        var response = await _client.GetAsync("/api/tasks");
+        response.EnsureSuccessStatusCode();
+        var taskItems = await response.Content.ReadFromJsonAsync<IEnumerable<TaskItemDetailsDto>>();
+        taskItems.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task GetTaskItemById_ReturnsOkResponse()
+    {
+        var response = await _client.GetAsync("/api/tasks/1");
+        response.EnsureSuccessStatusCode();
+        var taskItem = await response.Content.ReadFromJsonAsync<TaskItemDetailsDto>();
+        taskItem.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task CreateTaskItem_ReturnsCreatedResponse()
+    {
+        var newTaskItem = new TaskItemDto { Title = "Test Task", Description = "Test Description", DueDate = DateTime.UtcNow, Priority = Priority.Medium, Status = Status.Open };
+        var response = await _client.PostAsJsonAsync("/api/tasks", newTaskItem);
+        response.EnsureSuccessStatusCode();
+        var createdTaskItem = await response.Content.ReadFromJsonAsync<TaskItemDetailsDto>();
+        createdTaskItem.Should().NotBeNull();
+        createdTaskItem.Title.Should().Be("Test Task");
+    }
+
+    [Fact]
+    public async Task UpdateTaskItem_ReturnsNoContentResponse()
+    {
+        var updatedTaskItem = new TaskItemDto { Title = "Updated Task", Description = "Updated Description", DueDate = DateTime.UtcNow, Priority = Priority.High, Status = Status.InProgress };
+        var response = await _client.PutAsJsonAsync("/api/tasks/1", updatedTaskItem);
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task DeleteTaskItem_ReturnsNoContentResponse()
+    {
+        var response = await _client.DeleteAsync("/api/tasks/1");
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NoContent);
+    }
+}
